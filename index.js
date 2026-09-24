1754693b3e5bd7d37d7cc1ebf52985e0b316980a
Shoxrustam <shokhusa7@gmail.com>
2026-09-24 10:38:15 +0300

    vazifa qildim

---
+++ index.js
@@ -0,0 +1,88 @@
+// function yegindi(a,b){
+//     return a+b
+
+// }
+// console.log(yegindi(10,12));
+// // ===========================================================
+
+// function turdali (a){
+//     return a*60
+
+// }
+// console.log(turdali(5));
+// ================================================================
+
+// function haaa(a){
+//     return a+1
+
+// }
+// console.log(haaa(2));
+// =============================================================
+
+// function yuzi(a,b) {
+//   return (a*b)/2;
+// }
+// console.log(yuzi(5,6));
+// =====================================================
+
+// function yearToDays(age) {
+//   return age * 365;
+// }
+// console.log(yearToDays(10));
+// ===================================================
+
+// function firstElement(arr) {
+//     return arr[0];
+// }
+
+// console.log(firstElement([1, 2, 3])); // 1
+// console.log(firstElement([80, 5, 100])); // 80
+// console.log(firstElement([-500, 0, 50])); // -500
+======================================================================
+
+// function power(x, y) {
+//     return x * y;
+// }
+
+// console.log(power(230, 10)); // 2300
+// console.log(power(110, 3)); // 330
+// // console.log(power(480, 20)); // 9600
+// ===========================================================
+// function hourToSekunds(hour) {
+//     return hour * 60 * 60;
+// }
+
+// console.log(hourToSekunds(1)); // 3600
+// console.log(hourToSekunds(2)); // 7200
+// console.log(hourToSekunds(5)); // 18000
+// ===================================================================
+// function uchinchiTomon(tomon1, tomon2) {
+//     return (tomon1 + tomon2) - 1;
+// }
+
+// console.log(uchinchiTomon(8, 10)); // 17
+// console.log(uchinchiTomon(5, 7));  // 11
+// console.log(uchinchiTomon(9, 2));  // 10
+// ================================================================
+// function qoldiq(son1, son2) {
+//     return son1 % son2;
+// }
+
+// console.log(qoldiq(1, 3));    // 1
+// console.log(qoldiq(3, 4));    // 3
+// console.log(qoldiq(-9, 45));  // -9
+// console.log(qoldiq(5, 5));    // 0
+======================================================
+// function tortburchakYuzi(boyi, eni) {
+//     return boyi * eni;
+// }
+
+// console.log(tortburchakYuzi(5, 4)); // 20
+// console.log(tortburchakYuzi(10, 3)); // 30
+// ==================================================
+// function tortburchakYuzi(boyi, eni) {
+//     return 2 * (boyi + eni);
+// }
+// tortburchakYuzi(6, 7);   // 26
+// tortburchakYuzi(20, 10); // 60
+// tortburchakYuzi(2, 9);   // 22
